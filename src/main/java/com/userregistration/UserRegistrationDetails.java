9d216aaf48c9bf905816e33d241ffb876d7338a1
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-04-04 10:12:46 +0530

    ADDED: Added first name of user and checks valid or invalid

--- src/main/java/com/userregistration/UserRegistrationDetails.java
+++ src/main/java/com/userregistration/UserRegistrationDetails.java
@@ -1,11 +1,37 @@
 package com.userregistration;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserRegistrationDetails {
     Scanner scanner = new Scanner(System.in);
-    
+    private String firstName;
     public static void main(String[] args){
         System.out.println("Welcome to user registration problem ");
+        UserRegistrationDetails userRegistrationDetails = new UserRegistrationDetails();
+        String firstName = userRegistrationDetails.getFirstName();
+        userRegistrationDetails.registerFirstName(firstName);
+    }
+
+    /*Checking the first name of user is valid or invalid
+    *First name start with Upper case character
+    */
+    public String getFirstName(){
+        System.out.println("Enter your first name:");
+        firstName = scanner.nextLine();
+        return firstName;
+    }
+
+    public boolean registerFirstName(String firstName) {
+        Pattern pattern = Pattern.compile("[A-Z]{1}[a-z]{2}+", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean matches = matcher.find();
+        if(matches){
+            System.out.println("Your first name is valid");
+        } else {
+            System.out.println("Your first name is invalid");
+        }
+        return matches;
     }
 }
--- src/test/java/com/userregistration/UserRegistration.java
+++ src/test/java/com/userregistration/UserRegistration.java
@@ -1,5 +1,14 @@
 package com.userregistration;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserRegistration {
     UserRegistrationDetails userRegistrationDetails  = new UserRegistrationDetails();
+
+    @Test
+    public void giveFirstName_whereProper_shouldReturnTrue() {
+        boolean result = userRegistrationDetails.registerFirstName("Sujit");
+        Assert.assertEquals(result,true);
+    }
 }
